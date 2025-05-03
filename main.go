64e38877051377340ee01de9bf36c61efc582403
brunoroese <bruno.roese@functionize.com>
2025-05-03 17:21:34 -0300

    implement server for the network

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
-package socket_t1
+package main
 
-import "fmt"
+import (
+	"github.com/BrunoRoese/socket/config"
+	"github.com/BrunoRoese/socket/server"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("hello world")
+	config.SetupLogger()
+
+	shutdown := make(chan os.Signal, 1)
+	udpServer, err := server.Init("127.0.0.1", 8080)
+
+	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+
+	if err != nil {
+		slog.Error("Error starting server, stopping application", slog.String("error", err.Error()))
+		return
+	}
+
+	udpServer.StartListeningRoutine()
+
+	<-shutdown
+
+	udpServer.Close()
+
+	slog.Info("Shutting down server")
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,56 @@
 package server
+
+import (
+	"log/slog"
+	"net"
+)
+
+type Server struct {
+	udpAddr net.UDPAddr
+	conn    *net.UDPConn
+}
+
+func Init(ip string, port int) (*Server, error) {
+	var newServer Server
+
+	newServer.udpAddr = net.UDPAddr{IP: net.ParseIP(ip), Port: port}
+
+	slog.Info("Server initiating...", slog.String("ip", ip), slog.Int("port", port))
+
+	conn, err := net.ListenUDP("udp", &newServer.udpAddr)
+
+	if err != nil {
+		slog.Error("Error starting server", slog.String("ip", ip), slog.Int("port", port), slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	newServer.conn = conn
+
+	slog.Info("Server started", slog.String("ip", ip), slog.Int("port", port))
+
+	return &newServer, nil
+}
+
+func (s *Server) StartListeningRoutine() {
+	go func() {
+		for {
+			slog.Info("Waiting for message")
+			buffer := make([]byte, 1024)
+			n, addr, err := s.conn.ReadFromUDP(buffer)
+
+			if err != nil {
+				slog.Error("Error reading from UDP connection", slog.String("error", err.Error()))
+				continue
+			}
+
+			slog.Info("Received message", slog.String("message", string(buffer[:n])), slog.String("from", addr.String()))
+		}
+	}()
+}
+
+func (s *Server) Close() {
+	if err := s.conn.Close(); err != nil {
+		slog.Error("Error closing UDP connection", slog.String("error", err.Error()))
+	}
+	slog.Info("Server closed")
+}
