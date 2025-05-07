30a7f89f9acb37e6eb0e452641ea8027cb53cb68
brunoroese <bruno.roese@functionize.com>
2025-05-07 08:17:09 -0300

    add logic to handle counter for broadcast

--- pkg/server/counter.go
+++ pkg/server/counter.go
@@ -2,7 +2,7 @@ package server
 
 var requestsMap = make(map[string]int)
 
-func GetForIp(ip string) int {
+func GetByIp(ip string) int {
 	if count, ok := requestsMap[ip]; ok {
 		return count
 	}
@@ -10,7 +10,7 @@ func GetForIp(ip string) int {
 	return 0
 }
 
-func Increment(ip string) {
+func IncrementByIp(ip string) {
 	if count, ok := requestsMap[ip]; ok {
 		requestsMap[ip] = count + 1
 	} else {
--- pkg/server/handler/ack_handler.go
+++ pkg/server/handler/ack_handler.go
@@ -1,14 +1,45 @@
 package handler
 
 import (
+	"github.com/BrunoRoese/socket/pkg/client"
 	"github.com/BrunoRoese/socket/pkg/protocol"
+	"github.com/BrunoRoese/socket/pkg/protocol/parser"
+	"github.com/BrunoRoese/socket/pkg/server"
 	"github.com/google/uuid"
 	"log/slog"
 )
 
 func HandleAckReq(request *protocol.Request) {
-	if request.Information.Id == uuid.Nil() {
+	if request.Information.Id == uuid.Nil {
 		slog.Info("Broadcast ACK received", slog.String("source", request.Information.Source), slog.String("id", "0"))
 
+		ip, _, err := parser.ParseSource(request.Information.Source)
+
+		if err != nil {
+			slog.Error("Error parsing source", slog.String("error", err.Error()))
+			return
+		}
+
+		handleCounter(ip)
 	}
+}
+
+func handleCounter(ip string) {
+	counter := server.GetByIp(ip)
+	clientServiceSingleton := client.GetClientService()
+
+	if counter > 4 {
+		err := clientServiceSingleton.RemoveClientByIP(ip)
+
+		if err != nil {
+			slog.Error("Error removing ip: ", slog.String("ip", ip), slog.String("error", err.Error()))
+			return
+		}
+
+		slog.Info("Ip deleted successfully", slog.String("ip", ip))
+		return
+	}
+
+	slog.Info("Incrementing by ip: ", slog.String("ip", ip))
+	server.IncrementByIp(ip)
 }
