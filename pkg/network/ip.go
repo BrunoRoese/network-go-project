c11aaebc5425a51809076a9aa09207b2b38b9268
brunoroese <bruno.roese@functionize.com>
2025-05-07 08:35:17 -0300

    add logic to get local ip only once

--- pkg/network/ip.go
+++ pkg/network/ip.go
@@ -2,15 +2,23 @@ package network
 
 import "net"
 
+var localIp string
+
 func GetLocalIp() (string, error) {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if localIp == "" {
+		conn, err := net.Dial("udp", "8.8.8.8:80")
 
-	if err != nil {
-		return "", err
+		if err != nil {
+			return "", err
+		}
+		defer conn.Close()
+
+		localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+		localIp = localAddr.IP.String()
+
+		return localAddr.IP.String(), nil
 	}
-	defer conn.Close()
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP.String(), nil
+	return localIp, nil
 }
