639154c406f1f5ad01144918d4cb36d056c9acdc
brunoroese <bruno.roese@functionize.com>
2025-05-07 08:08:28 -0300

    add new file to count the number of request for each ip

--- pkg/server/counter.go
+++ pkg/server/counter.go
@@ -1,1 +1,19 @@
 package server
+
+var requestsMap = make(map[string]int)
+
+func GetForIp(ip string) int {
+	if count, ok := requestsMap[ip]; ok {
+		return count
+	}
+
+	return 0
+}
+
+func Increment(ip string) {
+	if count, ok := requestsMap[ip]; ok {
+		requestsMap[ip] = count + 1
+	} else {
+		requestsMap[ip] = 1
+	}
+}
