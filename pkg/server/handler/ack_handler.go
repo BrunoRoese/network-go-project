2eb7151bbd0fb8140a0855b91e8334224054c88e
brunoroese <bruno.roese@functionize.com>
2025-05-07 08:08:42 -0300

    create handler for ack type of request

--- pkg/server/handler/ack_handler.go
+++ pkg/server/handler/ack_handler.go
@@ -1,1 +1,14 @@
 package handler
+
+import (
+	"github.com/BrunoRoese/socket/pkg/protocol"
+	"github.com/google/uuid"
+	"log/slog"
+)
+
+func HandleAckReq(request *protocol.Request) {
+	if request.Information.Id == uuid.Nil() {
+		slog.Info("Broadcast ACK received", slog.String("source", request.Information.Source), slog.String("id", "0"))
+
+	}
+}
