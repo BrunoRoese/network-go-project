2a60f31d79b2c36012a331cfd34cf50ee9d16e85
brunoroese <bruno.roese@functionize.com>
2025-05-07 08:25:35 -0300

    send logs to file instead of terminal

--- config/log_config.go
+++ config/log_config.go
@@ -6,7 +6,16 @@ import (
 )
 
 func SetupLogger() {
-	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+	logFile, err := os.OpenFile("resources/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+	if err != nil {
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		})))
+		return
+	}
+
+	slog.SetDefault(slog.New(slog.NewJSONHandler(logFile, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
 	})))
 }
