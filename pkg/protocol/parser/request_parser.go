b5f931bc4b00a7bc8dbaf28b2a0785c752e8ef38
brunoroese <bruno.roese@functionize.com>
2025-05-07 08:08:12 -0300

    add new file to parse request

---
+++ pkg/protocol/parser/request_parser.go
@@ -0,0 +1,38 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/BrunoRoese/socket/pkg/protocol"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func ParseRequest(data []byte) (*protocol.Request, error) {
+	var req protocol.Request
+
+	err := json.Unmarshal(data, &req)
+
+	if err != nil {
+		log.Printf("Error parsing request: %v", err)
+		return nil, err
+	}
+
+	return &req, nil
+}
+
+func ParseSource(source string) (string, int, error) {
+	parts := strings.Split(source, ":")
+	if len(parts) != 2 {
+		return "", 0, fmt.Errorf("invalid source format: %s", source)
+	}
+
+	ip := parts[0]
+	port, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return "", 0, fmt.Errorf("invalid port: %s", parts[1])
+	}
+
+	return ip, port, nil
+}
