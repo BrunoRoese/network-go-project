268ee232369b3e3ba3cb972a9217ff0487fadd1e
brunoroese <bruno.roese@functionize.com>
2025-05-08 20:59:01 -0300

    create new command to start file transfer

---
+++ cmd/file.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"github.com/BrunoRoese/socket/pkg/protocol/service"
+	"log/slog"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	filePath string
+)
+
+var fileCmd = &cobra.Command{
+	Use:   "file",
+	Short: "Indicates the start of a file transfer to a client",
+	Long:  `Indicates the start of a file transfer to a client.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fileService := service.FileService{}
+
+		err := fileService.StartTransfer(clientIp, filePath)
+
+		if err != nil {
+			slog.Error("Error starting file transfer", slog.String("error", err.Error()))
+			return
+		}
+	},
+}
+
+func init() {
+	fileCmd.PersistentFlags().StringVar(&filePath, "filePath", "", "The directory of the file to be sent")
+	err := fileCmd.MarkPersistentFlagRequired("file")
+
+	fileCmd.PersistentFlags().StringVar(&clientIp, "ip", "", "The client ip to send the file")
+	err = fileCmd.MarkPersistentFlagRequired("ip")
+
+	rootCmd.AddCommand(fileCmd)
+
+	if err != nil {
+		slog.Error("Error marking flag as required", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+}
